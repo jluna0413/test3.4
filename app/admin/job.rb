cc7cce06b60a38296329ce0712b9c56006769a9e
Prelang Builder <builder@prelang.com>
2015-10-26 17:45:22 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Job' to allow for create, update

--- app/admin/job.rb
+++ app/admin/job.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Job do
+  permit_params :description, :jobstart, :jobend, :talenttype, :payrate, :jobzip, :jobaddress
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
