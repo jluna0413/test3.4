b40d68b3c20ba0d4f4b3a2bc423afe85ed715ed3
Prelang Builder <builder@prelang.com>
2015-10-26 17:45:16 +0000

    Created migration for HABTM table between 'Job' and 'User'

---
+++ db/migrate/20151026174515_create_jobs_users.rb
@@ -0,0 +1,8 @@
+class CreateJobsUsers < ActiveRecord::Migration
+  def change
+    create_table :jobs_users do |t|
+      t.belongs_to :job, index: true
+      t.belongs_to :user, index: true
+    end
+  end
+end
